7038bedde03e02fc2da85b155cccc9aabf93b863
abdelrahmansmm <abdelrahman.shoukry9@gmail.com>
2025-11-12 20:22:33 +0200

    Create ddl_bronze_layer.sql

---
+++ scripts/bronze/ddl_bronze_layer.sql
@@ -0,0 +1,65 @@
+-- Creating the tables in bronze schema.
+-- Creating the bronze tables exactly like the source CSV files.
+-- Creating code checks first if the table exists and drops it, then it creates a new one.
+
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info (
+	cst_id INT,
+	cst_key NVARCHAR(255),
+	cst_firstname NVARCHAR(255),
+	cst_lastname NVARCHAR(255),
+	cst_marital_status NVARCHAR(255),
+	cst_gndr NVARCHAR(255),
+	cst_create_date DATE
+);
+
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info (
+	prd_id INT,
+	prd_key NVARCHAR(255),
+	prd_nm NVARCHAR(255),
+	prd_cost INT,
+	prd_line NVARCHAR(255),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
+);
+
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num NVARCHAR(255),
+	sls_prd_key NVARCHAR(255),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
+);
+
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12 (
+	cid NVARCHAR(255),
+	bdate DATE,
+	gen NVARCHAR(255)
+);
+
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
+	cid NVARCHAR(255),
+	cntry NVARCHAR(255)
+);
+
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+	id NVARCHAR(255),
+	cat NVARCHAR(255),
+	subcat NVARCHAR(255),
+	maintenance NVARCHAR(255)
+);
